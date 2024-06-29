41685db0969b7664c6afda043234c1be7869a70a
Oscar Valois <118651508+osvalois@users.noreply.github.com>
2024-06-29 16:28:14 -0600

    add: Method for serving static files in a generic way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,58 +1,34 @@
-require 'sinatra/base'
-
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, File.expand_path('../../public', __dir__)
     set :views, File.expand_path('../../public/scripts/core/presentation/views', __dir__)
   end
 
-  # Ruta para servir el formulario de creación de usuario
-  get '/usuarios/crear' do
-    begin
-      file_path = File.join(settings.public_folder, 'scripts/usuarios/presentation/views/crearUsuario.html')
-      logger.info "Trying to send file: #{file_path}"
-
-      if File.exist?(file_path)
-        send_file file_path
-      else
-        logger.error "File not found: #{file_path}"
-        raise Errno::ENOENT
-      end
-    rescue Errno::ENOENT
-      logger.error "File crearUsuario.html not found in public directory: #{file_path}"
-      status 404
-      content_type :html
-      erb :not_found, layout: false, locals: { file_path: request.path_info }
+  # Método para servir archivos estáticos de manera genérica
+  def serve_static_file(file_path)
+    if File.exist?(file_path)
+      send_file file_path
+    else
+      logger.error "File not found: #{file_path}"
+      raise Errno::ENOENT
     end
-  end
-
-  # Ruta para servir el archivo index.html
-  get '/' do
-    begin
-      file_path = File.join(settings.public_folder, 'index.html')
-      logger.info "Trying to send file: #{file_path}"
-
-      if File.exist?(file_path)
-        send_file file_path
-      else
-        logger.error "File not found: #{file_path}"
-        raise Errno::ENOENT
-      end
-    rescue Errno::ENOENT
-      logger.error "File index.html not found in public directory: #{file_path}"
-      status 404
-      content_type :html
-      erb :not_found, layout: false, locals: { file_path: request.path_info }
-    end
-  end
-
-  # Handler para errores 404
-  not_found do
+  rescue Errno::ENOENT
+    logger.error "File not found in public directory: #{file_path}"
     status 404
     content_type :html
     erb :not_found, layout: false, locals: { file_path: request.path_info }
   end
-end
 
-# Iniciar la aplicación Sinatra si se ejecuta este archivo directamente
-run ApplicationController if __FILE__ == $0
+  get '/usuarios' do
+    serve_static_file(File.join(settings.public_folder, 'scripts/usuarios/presentation/views/crearUsuario.html'))
+  end
+
+  get '/' do
+    serve_static_file(File.join(settings.public_folder, 'index.html'))
+  end
+
+  # Manejador genérico para errores 404
+  not_found do
+    serve_static_file(File.join(settings.public_folder, '404.html'))
+  end
+end
